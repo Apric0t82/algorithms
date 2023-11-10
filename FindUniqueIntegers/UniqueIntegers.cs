7a77bb8fa24cabbe085f35eaa49f9745309ac78b
Alex <apric0t@outlook.com>
2023-11-10 19:46:11 +0300

    unit test added

---
+++ FindUniqueIntegers/UniqueIntegers.cs
@@ -0,0 +1,55 @@
+﻿namespace Algorithms;
+
+public class UniqueIntegers
+{
+    public List<int> GetUniqueIntegers(int[] array) {
+        var frequencyDict = new Dictionary<int, int>();
+        var uniqueInts = new List<int>();
+
+        foreach (var num in array)
+        {
+            if (frequencyDict.ContainsKey(num)){
+                frequencyDict[num]++;
+            } else {
+                frequencyDict.Add(num, 1);
+            }
+        }
+
+        foreach (var kvp in frequencyDict)
+        {
+            if (kvp.Value == 1) {
+                uniqueInts.Add(kvp.Key);
+            }
+        }
+
+        return uniqueInts;
+    }
+
+    public List<int> GetUniqueIntegersAlternative(int[] array) {
+        var uniqueInts = new List<int>();
+
+        Array.Sort(array);
+
+        var length = array.Length;
+        var i = 0;
+
+        while (i < length)
+        {
+            var current = array[i];
+            var count = 0;
+
+            while (i < length && array[i] == current)
+            {
+                count++;
+                i++;
+            }
+
+            if (count == 1) {
+                uniqueInts.Add(current);
+            }
+        }
+
+        return uniqueInts;
+    }
+
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+using Algorithms;
+
+namespace FindUniqueIntegers.Tests;
+
+public class UnitTest1
+{
+
+    // TODO test edge cases
+    /*
+       The list is huge (i.e. several billion numbers or more)
+       The list is tiny (0 or 1 items)
+       The list is null
+       Everything is a duplicate
+       Everything is unique
+       The list is just the same number repeated many times
+    */
+
+    [Fact]
+    public void CanReturnListOfUniqueIntegers()
+    {
+        // Arrange
+        var uniqueInts = new UniqueIntegers();
+        var array = new int[] {1, 10, -4, 2, 7, 8, 45, -32, 2, 9, -4, 33};
+
+        // Act
+        var list = uniqueInts.GetUniqueIntegers(array);
+
+        // Assert
+        Assert.Equal("1,10,7,8,45,-32,9,33", string.Join(",", list));
+    }
+
+    [Fact]
+    public void CanReturnSortedListOfUniqueIntegers()
+    {
+        // Arrange
+        var uniqueInts = new UniqueIntegers();
+        var array = new int[] {1, 10, -4, 2, 7, 8, 45, -32, 2, 9, -4, 33};
+
+        // Act
+        var list = uniqueInts.GetUniqueIntegersAlternative(array);
+
+        // Assert
+        Assert.Equal("-32,1,7,8,9,10,33,45", string.Join(",", list));
+    }
+}
