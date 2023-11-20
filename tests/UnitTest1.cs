4fd47fce695b8ace72bec60bbaccdc29e56f624b
Alex <apric0t@outlook.com>
2023-11-20 18:12:15 +0300

    cleanup

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -12,12 +12,8 @@ public class UnitTest1
     [Fact]
     public void TestListIsEmpty()
     {
-        // Arrange
-
-        // Act
         var list = UniqueIntegers.WithDictionary(Array.Empty<int>());
 
-        // Assert
         Assert.Empty(list);
     }
 
@@ -25,12 +21,8 @@ public class UnitTest1
     [MemberData(nameof(UniqueIntegers.Data), MemberType = typeof(UniqueIntegers))]
     public void Test_CanReturnWithExceptingDuplicatesHashSet(int[] array, List<int> expected)
     {
-        // Arrange
-
-        // Act
         var list = UniqueIntegers.WithExceptingDuplicatesHashSet(array);
 
-        // Assert
         Assert.Equal(expected, list);
     }
 
@@ -38,12 +30,8 @@ public class UnitTest1
     [MemberData(nameof(UniqueIntegers.Data), MemberType = typeof(UniqueIntegers))]
     public void Test_CanReturnWithDictionary(int[] array, List<int> expected)
     {
-        // Arrange
-
-        // Act
         var list = UniqueIntegers.WithDictionary(array);
 
-        // Assert
         Assert.Equal(expected, list);
     }
 
@@ -51,12 +39,8 @@ public class UnitTest1
     [MemberData(nameof(UniqueIntegers.SortedData), MemberType = typeof(UniqueIntegers))]
     public void Test_CanReturnWithSortAndLinearScan(int[] array, List<int> expected)
     {
-        // Arrange
-
-        // Act
         var list = UniqueIntegers.WithSortAndLinearScan(array);
 
-        // Assert
         Assert.Equal(expected, list);
     }
 }
